66c9946fc78da8b52e619e7dbc7a7f62f05e9d38
Amit Dubey <amitdubey6261@gmail.com>
2021-12-05 13:58:51 +0530

    haspath path full

--- haspath.cpp
+++ haspath.cpp
@@ -19,6 +19,23 @@ void display(vector<vector<edge*>> G , int*arr){
         }
     }
 }
+
+bool haspath(vector<vector<edge*>> G , int src , int dest , bool visited[]){
+    if(src == dest ){
+        return true;
+    }
+    visited[src] = true;
+    for(int i=0 ; i<G[src].size() ; i++){
+        if(visited[G[src][i]->neg] == false){
+            bool hasnbrpath = haspath(G , G[src][i]->neg , dest , visited);
+            if (hasnbrpath == true){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 int main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt" , "r" , stdin);
@@ -28,6 +45,7 @@ int main(){
     int nv;
     cin>>nv;
     int arr[] = {2,2,2,3,3,2,2};
+    bool visited[sizeof(arr)/sizeof(arr[0])];
     vector<vector<edge*>> G(nv , vector<edge*>());
     for(int i=0  ; i<nv ; i++){
         for(int j=0 ; j<arr[i] ; j++){
@@ -37,5 +55,6 @@ int main(){
         }
     }
     display(G , arr);
+    cout<<"Haspath :"<<haspath(G , 0 ,909 ,visited);
     return 0;  
 }
